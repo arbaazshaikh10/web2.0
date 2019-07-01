50faf631e83fb48f57c9aa437400eb97040bdb48
unknown <abzsk42@gmail.com>
2019-07-01 22:48:28 +0530

    Validation Message

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ app.use(bodyParser.json());
 app.post('/',(req,res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
+
+  if(amount <= 1){
+    return_info ={};
+    return_info.error = true;
+    return_info.message = "The amount should be greater than 1";
+    return res.send(return_info);
+  }
+
   res.send({"amount":amount, "email": email});
-})
+});
 
 app.listen(3000,()=>{
   console.log('Server is running on Port 3000');
