eee3aec2b8f0429714e90d4ee729311f399ac024
unknown <abzsk42@gmail.com>
2019-07-30 15:55:14 +0530

    Adding fields to view

---
+++ public/index.js
@@ -0,0 +1,33 @@
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      "total_amount" :1000
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>LOTTERY APPLICATION</h1>
+        <div>
+          <p>Total lottery amount is {this.state.total_amount}</p>
+        </div>
+
+        <form>
+          <input placeholder ="amount" />
+          <input placeholder ="email" />
+          <button> Participate </button>
+        </form>
+      </div>
+    )
+  }
+};
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>
+  , document.getElementById('reactBinding')
+);
