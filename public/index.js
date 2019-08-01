76716d888aa18f4b5ddaaa598d9d4428bb25b81f
unknown <abzsk42@gmail.com>
2019-08-01 23:18:17 +0530

    Creating payments with PayPal

--- public/index.js
+++ public/index.js
@@ -20,7 +20,7 @@ class App extends React.Component{
       amount:this.state.amount,
       email:this.state.email
     })
-    console.log(response);
+    window.location.href = response.data;
   }
 
   render(){
